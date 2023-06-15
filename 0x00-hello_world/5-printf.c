505615251666ad64cb9fba3cbe41392c3385ec95
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-15 15:22:20 -0700

    Hello, printf tasks completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if sucess.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\");
+
+	return (0);
+}
