7502586ed8062c754bb3240dcf5a9e0f452ad239
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-15 16:07:09 -0700

    hello, puts tasks completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ * 
+ * Return: Always  0 (success)
  */
 int main(void)
 
