2ba3ec9224e04be99473964cd05d2c69967764e3
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-16 06:36:28 -0700

    The last digit tasks completed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		printf("last digit of %d is %d is greater than 5\n", n, n % 10);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if ((n % 10) < 6 && (n % 10) != 0);
 	{
 		printf("last digit of %d is %d is less than 6 and not 0\n", n, n % 10);
 	}
