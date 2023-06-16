b6837dbd34426b0030bd6a46a8ac0c1548b7dbcd
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-16 10:06:08 -0700

    Hexadecimal tasks completed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int numbers;
-        char alphabet;
+	char alphabet;
 
-        for (numbers = 0; numbers < 10; numbers++)
-                putchar((numbers % 10) + '0');
+	for (numbers = 0; numbers < 10; numbers++)
+		putchar((numbers % 10) + '0');
 
 	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
-                putchar(alphabet);
+		putchar(alphabet);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
