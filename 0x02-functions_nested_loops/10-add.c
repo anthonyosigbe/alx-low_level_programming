7004a54174e0e90f29f1c08e765d38671f078793
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-20 08:44:30 -0700

    a + b tasks completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result.
+ * @a1: first integer to be added.
+ * @a2: second integer to be added.
+ *
+ * Return: result of a1 + a2
+ */
+int add(int a1, int a2)
+{
+	return (a1 + a2);
+}
