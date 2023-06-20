b7fcb2766fe67f30767bfb0856a02e8af5647095
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-20 06:28:28 -0700

    islower tasks completed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: Character that needs to be checked
+ *
+ * Return: 1 if c is lowercase or 0 for otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+
+	return (0);
+}
