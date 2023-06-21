13cd78fc0253666a5b855c95dc2ac3607e01c07f
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-21 07:16:43 -0700

    0 > 972? tasks completed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >=b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (c > b && b > a) 
-	{
-		largest =  c;
-	}
 	else
 	{
-		largest = a;
+		largest = c;
 	}
 
 	return (largest);
