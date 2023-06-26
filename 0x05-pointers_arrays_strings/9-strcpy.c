743a55aafaaed9d92a37b5c1c4d706ca590e509e
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-26 12:02:45 -0700

    strcpy tasks completed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ *
+ * @dest: Buffer that the sring will be copied to.
+ * @src: Source string to copy.
+ *
+ * Return: Pointer to string *dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
