3dae975b7cd7decb48daafc077afbf74b139c631
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-26 07:10:54 -0700

    98 Battery s tasks completed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter,
+ * and updates the value it points to 98.
+ *
+ * @n: Pointer to an integer (int).
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
