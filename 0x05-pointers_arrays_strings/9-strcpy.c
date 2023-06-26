fed91a1bd00fc2d5d30845f66d326e37ff331483
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-26 12:10:19 -0700

    strcpy tasks completed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,11 +16,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+
+	dest[i++] = '\0';
 
 	return (dest);
 }
