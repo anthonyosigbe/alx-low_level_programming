7431b962e559c50f7344a35ad3e156e82c335e41
Anthony Osigbe <thronesofglory@gmail.com>
2023-06-29 09:23:47 -0700

    correction of Don't hate the hacker, hate the code tasks done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,30 +7,45 @@
  *
  * Return: Always 0 (Success).
  */
-
 int main(void)
 {
-	int pwd[120];
-	int i, add, n;
+	char generated_password[84];
+	int position = 0, total = 0, set1, set2;
 
-	add = 0;
-
-	srand(time(NULL));
-
-	for (i = 0; i < 120; i++)
+	srand(time(0));
+	while (total < 2772)
 	{
-		pwd[i] = rand() % 98;
-		add += (pwd[i] + '0');
-		putchar(pwd[i] + '0');
+		generated_password[position] = 33 + rand() % 94;
+		total += generated_password[position++];
+	}
+	generated_password[position] = '\0';
 
-		if ((2772 - add) - '0' < 98)
+	if (total != 2772)
+	{
+		set1 = (total - 2772) / 2;
+		set2 = (total - 2772) / 2;
+
+		if ((total - 2772) % 2 != 0)
+			set1++;
+
+		for (position = 0; generated_password[position]; position++)
 		{
-			n = 2772 - add - '0';
-			add += n;
-			putchar(n + '0');
-			break;
+			if (generated_password[position] >= (33 + set1))
+			{
+				generated_password[position] -= set1;
+				break;
+			}
+		}
+		for (position = 0; generated_password[position]; position++)
+		{
+			if (generated_password[position] >= (33 + set2))
+			{
+				generated_password[position] -= set2;
+				break;
+			}
 		}
 	}
+	printf("%s", generated_password);
 
 	return (0);
 }
