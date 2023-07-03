7db5dea0ef60272dfedef045827434655ca12716
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-03 06:04:36 -0700

    strchr tasks completed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr -  locates a character in a string.
@@ -19,5 +18,5 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 			return (s + i);
 	}
-	return (NULL);
+	return (0);
 }
