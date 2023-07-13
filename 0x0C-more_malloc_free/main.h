304b555d3f564c46c1b85a5e55d8ac325bf24226
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-13 09:16:45 -0700

    main.h updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,8 +8,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int multiply(int num1, int num2);
-int validateInput(const char *str);
-int main(int argc, char *argv[]);
-int multiply(int num1, int num2)
+
 #endif
