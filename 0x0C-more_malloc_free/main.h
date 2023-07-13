739ff55f08af92151e688effd3ad1a36c57af16a
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-13 09:52:35 -0700

    main.h file updated with the newly generated prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+int stringLength(char *str);
+char *createCharArray(int size);
+char *removeLeadingZeroes(char *str);
+int getDigit(char c);
+void multiplyByDigit(char *result, char *number, int digit, int leadingZeroes);
+void addNumbers(char *sum, char *nextNum, int nextLength);
 #endif
