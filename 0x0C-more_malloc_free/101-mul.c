7e6576e63c7fb301cb2280e32c4ab126e7876ac1
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-13 07:01:31 -0700

    5. We must accept finite disappointment, but never lose infinite hope- advanced tasks completed

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * multiply - Multiplies two integers.
+ * @num1: The first integer.
+ * @num2: The second integer.
+ *
+ * Return: The product of num1 and num2.
+ */
+
+int multiply(int num1, int num2);
+
+/**
+ * validateInput - Validates if a string contains only digits.
+ * @str: The string to validate.
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise.
+ */
+
+int validateInput(const char *str);
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Description: Multiplies two positive numbers provided as,
+ * command-line arguments,
+ * The program checks if the correct number of arguments is provided,
+ * and if each argument contains only digits.
+ * It then converts the arguments to integers,
+ * calculates their product using the multiply function,
+ * and prints the result.
+ *
+ * Return: 0 on success, or 98 if the number of arguments is incorrect or,
+ * the arguments contain non-digits. Print Error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (!validateInput(argv[1]) || !validateInput(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+ * multiply - Multiplies two integers.
+ * @num1: The first integer.
+ * @num2: The second integer.
+ *
+ * Return: The product of num1 and num2.
+ */
+
+int multiply(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * validateInput - Validates if a string contains only digits.
+ * @str: The string to validate.
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise.
+ */
+
+int validateInput(const char *str)
+{
+	while (*str != '\0')
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
