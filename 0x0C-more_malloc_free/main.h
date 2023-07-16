40049824b122f6adb42606707593a36a9614b6e0
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-16 08:50:40 -0700

    main.h file update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,10 +7,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int stringLength(char *str);
-char *createCharArray(int size);
-char *removeLeadingZeroes(char *str);
-int getDigit(char c);
-void multiplyByDigit(char *result, char *number, int digit, int leadingZeroes);
-void addNumbers(char *sum, char *nextNum, int nextLength);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif
