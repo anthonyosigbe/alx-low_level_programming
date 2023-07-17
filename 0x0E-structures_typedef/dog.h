9293dd36338aac3ce4033a253cff466198aa3dfe
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-17 08:33:27 -0700

    0. Poppy tasks completed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog- Defines a custom data type representing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
