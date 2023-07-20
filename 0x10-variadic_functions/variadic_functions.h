7c619d6a9799f170b965d2556dbf56fd0ec5a461
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-20 05:06:43 -0700

    variadic_functions.h file update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,23 +3,6 @@
 
 #include <stdarg.h>
 
-/**
- * struct PrintFormat - A new struct type defining a printer,
- * for different data types.
- * @format_specifier: A symbol representing the format,
- * specifier for a particular data type.
- * @print: A function pointer to a function that prints the data type,
- * corresponding to the format specifier.
- */
-
-typedef struct printer
-{
-	char *format_specifier;
-        void (*print)(va_list arg);
-} printer_t;
-
-
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
