13ddd60b59d12be88cc0b3e1f3594681fe1b7d31
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-20 02:13:58 -0700

    variadic_functions.h file created and updated with tasks prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_UTILS_H
+#define VARIADIC_UTILS_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
