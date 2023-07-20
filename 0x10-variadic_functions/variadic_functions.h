11a2a187e511bbb1376554c9724715fa36287b4a
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-20 04:10:29 -0700

    prototypes updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct PrintFormat - A new struct type defining a printer for different data types.
+ * @format_specifier: A symbol representing the format specifier for a particular data type.
+ * @print: A function pointer to a function that prints the data type,
+ * corresponding to the format specifier.
+ */
+
+typedef struct PrintFormat
+{
+	char *format_specifier;
+	void (*print)(va_list arg);
+} PrintFormat_t;
+
 #endif
