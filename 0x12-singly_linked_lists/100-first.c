026c4f1b9e450517dbdf749ab90ac0423b0ea66c
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-27 04:47:26 -0700

    5. The Hare and the Tortoise tasks completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * hare_constructor - Constructor function to print,
+ * a message before main execution.
+ */
+void hare_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
