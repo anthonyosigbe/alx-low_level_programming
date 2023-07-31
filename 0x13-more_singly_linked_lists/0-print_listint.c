d7878d626e2c28da7ab5abc1b384ff4f958ea197
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-31 04:30:13 -0700

    0. Print list tasks completed

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t elementCount = 0;
+
+	while (h)
+	{
+		elementCount++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (elementCount);
+}
