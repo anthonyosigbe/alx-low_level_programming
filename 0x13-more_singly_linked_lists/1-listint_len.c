279f561bdff37e983ff5be5e23fbb24d57c9273d
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-31 12:43:24 -0700

    1. List length tasks completed

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Counts the number of elements in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t elementCount = 0;
+
+	while (h != NULL)
+	{
+		elementCount++;
+		h = h->next;
+	}
+	return (elementCount);
+}
+
