a15a69c9321cceea189e176585e2ee1df7a368bd
Anthony Osigbe <thronesofglory@gmail.com>
2023-08-07 09:01:11 -0700

    main.h file update with 3. cp tasks prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
+char *allocate_temp_buffer(char *file);
+void close_file_descriptor(int fd);
+void read_write_files(int source_fd, int dest_fd);
+int main(int argc, char *argv[]);
 
 #endif
