2300ae93cc24cd8a290cd561bae9a2b0d97c2c70
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-05 04:19:05 -0700

    3. >>> ht['betty'] = 'cool' tasks completed

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add an element to a hash table.
+ * @ht: The hash table to add to.
+ * @key: The key to add (cannot be an empty string).
+ * @value: The value to associate with the key (can be an empty string).
+ *
+ * Return: 1 if successful, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
