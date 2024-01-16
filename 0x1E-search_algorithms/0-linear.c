2e136acc3360478e354a1047f3effbca4ad1f4dc
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-15 22:33:15 -0800

    0. Linear search tasks completed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,16 +15,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	int index;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < (int)size; i++)
+	for (index = 0; index < (int)size; index++)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
+		printf("Value checked array[%u] = [%d]\n", index, array[index]);
+		if (value == array[index])
+			return (index);
 	}
 	return (-1);
 }
