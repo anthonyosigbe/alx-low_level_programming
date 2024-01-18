2e40d4a9fd76506b939c19f394d496bab7a028d7
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-18 03:15:47 -0800

    Correction of 0-linear.c file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,16 +15,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int index;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
-	for (index = 0; index < (int)size; index++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%u] = [%d]\n", index, array[index]);
-		if (value == array[index])
-			return (index);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
+
 	return (-1);
 }
